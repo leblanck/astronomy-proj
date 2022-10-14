d18c5cc2c02b9c46e26943e60eb889a3642ce228
Kyle LeBlanc <kyle@leblanc.sh>
2022-10-14 11:21:02 -0400

    minimal requirements have been met. print single local to console

--- astronomy_proj/solar.py
+++ astronomy_proj/solar.py
@@ -18,8 +18,12 @@ def get_observer_location():
 
     lat = data["lat"]
     lon = data["lon"]
+    city = data["city"]
+    state = data["region"]
 
-    return lat, lon
+    local = f"From {city}, {state}:"
+
+    return lat, lon, local
 
 
 def get_sun_position(lat, lon):
@@ -43,17 +47,28 @@ def get_sun_position(lat, lon):
         auth=basic,
         params=payload,
     )
-    data = response.text
+    data = response.json()
+    azi = data["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"][
+        "azimuth"
+    ]["string"]
 
-    print(data)
+    alt = data["data"]["table"]["rows"][0]["cells"][0]["position"]["horizontal"][
+        "altitude"
+    ]["string"]
+
+    return azi, alt
 
 
-# def print_position(azimuth, altitude):
-#     pass
+def print_position(azi, alt, loc):
+    """
+    print to console
+    """
+    print(
+        f"{loc}\n The Sun is currently at: {azi} degress azimuth and {alt} degress altitude."
+    )
 
 
 if __name__ == "__main__":
-    latitude, longitude = get_observer_location()
-    # azimuth, altitude = get_sun_position()
-    get_sun_position(latitude, longitude)
-    # print_position(azimuth, altitude)
+    latitude, longitude, location = get_observer_location()
+    azimuth, altitude = get_sun_position(latitude, longitude)
+    print_position(azimuth, altitude, location)
