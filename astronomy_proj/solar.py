36fd710e680797bbe9d62b1112a5e875f74359cd
Kyle LeBlanc <kyle@leblanc.sh>
2022-10-14 10:41:34 -0400

    feat: created initial skeleton funcs

---
+++ astronomy_proj/solar.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+find the sun
+"""
+
+import datetime as dt
+import requests
+from requests.auth import HTTPBasicAuth
+from astro_keys import ASTRONOMY_ID, ASTRONOMY_SECRET
+
+
+def get_observer_location():
+    """
+    this will get loc
+    """
+    response = requests.get("http://ip-api.com/json/")
+    data = response.json()
+
+    lat = data["lat"]
+    lon = data["lon"]
+
+    return lat, lon
+
+
+def get_sun_position(lat, lon):
+    """
+    get sun loc
+    """
+    date = dt.date.today()
+    now = dt.datetime.now()
+    time = now.strftime("%H:%M:%S")
+    basic = HTTPBasicAuth(ASTRONOMY_ID, ASTRONOMY_SECRET)
+    payload = {
+        "latitude": lat,
+        "longitude": lon,
+        "elevation": "0",
+        "from_date": date,
+        "to_date": date,
+        "time": time,
+    }
+    response = requests.get(
+        "https://api.astronomyapi.com/api/v2/bodies/positions/sun",
+        auth=basic,
+        params=payload,
+    )
+    data = response.text
+
+    print(data)
+
+
+# def print_position(azimuth, altitude):
+#     pass
+
+
+if __name__ == "__main__":
+    latitude, longitude = get_observer_location()
+    # azimuth, altitude = get_sun_position()
+    get_sun_position(latitude, longitude)
+    # print_position(azimuth, altitude)
